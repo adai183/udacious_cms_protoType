b9b9579ce418349616db8800f53ee4ab9610d4cd
Tamás Krasser <krasser.tamas@gmail.com>
2016-08-23 15:09:55 +0200

    change(routes): use browserHistory
    browserHistory doesn't pollute the url with random strings

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { MainContainer } from 'containers'
 import { Feed, CmsEditor } from 'components'
 
 const routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={MainContainer}>
       <IndexRoute component={Feed} />
       <Route path='/cms' component={CmsEditor} />
